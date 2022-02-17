97004ff0a43b06a9b1da670cf447d648e595331a
AiswaryaGanapathy <aiswarya.g.ishu@gmail.com>
2022-02-17 11:22:44 +0530

    Create day3-tasktwo.js

---
+++ day3-tasktwo.js
@@ -0,0 +1,41 @@
+let resume =
+    {"Personal Details" : {
+        "name": "Aiswarya Ganapathy",
+            "email": "xxx@gmail.com",
+      "phone": "000000000",
+      "degree": "B.E",
+      "location":  {
+        "address": "xxx street",
+        "postalCode": "000000",
+        "city": "Tamilnadu",
+        "countryCode": "+91",
+        "region": "Hindu"
+      }},
+
+    "Education":[
+        {
+        "Institution": "xxxx engineening college",
+        "Department": "Electrical and Electronics Engineering",
+        "startDate": "7.9.2009",
+        "endDate": "28.5.2013",
+         "courses": [
+          "B.E - Electrical and Electronics Engineering"
+    ]
+}
+    ],
+    "Skills": [
+        {
+          "Languages": "C,C++,Java",
+          "level": "basic",
+        }
+      ],
+      "languages": [
+        {
+          "language": "English , Tamil",
+          "fluency": "Advanced"
+        }
+      ]
+    }
+console.log(resume);
+
+
